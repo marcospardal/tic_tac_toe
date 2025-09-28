2e04f20fb8f0a3a2843b68ffdc367e33159910b8
Pardal <marcospardalnascimento@gmail.com>
2025-09-28 12:53:00 -0300

    Add home page content

--- webclient/src/app/pages/Home/home.component.ts
+++ webclient/src/app/pages/Home/home.component.ts
@@ -1,15 +1,17 @@
-import { Component } from "@angular/core";
-import { Auth } from "../../services/auth";
+import { Component } from '@angular/core';
+import { Auth } from '../../services/auth';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCard } from '@angular/material/card';
 
 @Component({
   selector: 'home-page',
   templateUrl: './home.html',
   standalone: true,
-  styleUrl: './home.sass'
+  styleUrl: './home.sass',
+  imports: [MatButtonModule, MatCard],
 })
 export class HomePage {
-  constructor (private auth: Auth) {
-    console.log('token', auth.getToken())
+  constructor(private auth: Auth) {
+    console.log('token', auth.getToken());
   }
-
 }
